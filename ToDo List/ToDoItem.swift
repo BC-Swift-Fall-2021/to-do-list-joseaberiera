bfeebd56504f38f607187b09d33052bc820d022f
Jose Riera <rieraja@bc.edu>
2021-09-27 13:45:41 -0400

    5.7

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by Jose Riera on 9/27/21.
+//
+
+import Foundation
+
+struct ToDoItem: Codable{
+    var name: String
+    var date: Date
+    var notes: String
+}
